b4092c7a460dcb68ab1dc9a888f5ffdc029dbaf3
Patrick Nance <jpnance@gmail.com>
2012-08-17 19:51:57 -0700

    Adding the first users.js file which will be super-modified as we go on.

---
+++ data/users.js
@@ -0,0 +1,1 @@
+db.users.save({ "username": "jpnance", "firstName": "Patrick", "lastName": "Nance", "password": "05f60ba25a20f67f442f4127e4d6c4dd", "seasons": [ 2008, 2010, 2011 ], "nickname": "Pat" });

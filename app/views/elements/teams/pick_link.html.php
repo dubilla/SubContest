c0b6024a1d743097410065bfbe10f7f45769f11a
Patrick Nance <jpnance@gmail.com>
2012-08-19 20:34:06 -0700

    Adding some bare-bones CSS for team pick links.

--- app/views/elements/teams/pick_link.html.php
+++ app/views/elements/teams/pick_link.html.php
@@ -1,6 +1,6 @@
 <?php if (isset($game->picks[$username]) && $game->picks[$username] == $team->abbreviation): ?>
-	<?= $this->html->link($team->location, 'games/unpick/' . $game->_id); ?>
+	<?= $this->html->link($team->location, 'games/unpick/' . $game->_id, array('class' => 'team picked')); ?>
 <?php else: ?>
-	<?= $this->html->link($team->location, 'games/pick/' . $game->_id . '/' . $team->abbreviation); ?>
+	<?= $this->html->link($team->location, 'games/pick/' . $game->_id . '/' . $team->abbreviation, array('class' => 'team')); ?>
 <?php endif; ?>
 

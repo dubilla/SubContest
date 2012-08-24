c598f25d875d370ad5b59ef6af2aeddd49836276
Patrick Nance <jpnance@gmail.com>
2012-08-24 11:40:57 -0700

    Adding a nice little div so we can clearly show who's the home team but keep everything aligned well.

--- app/views/elements/games/not_started.html.php
+++ app/views/elements/games/not_started.html.php
@@ -1,4 +1,7 @@
 <div class="game not-started">
-	<?= $this->view()->render(array('element' => 'teams/pick_link'), array('game' => $game, 'team' => $game->awayTeam(), 'username' => 'jpnance')); ?><br /> 
-	<?= $this->view()->render(array('element' => 'teams/pick_link'), array('game' => $game, 'team' => $game->homeTeam(), 'username' => 'jpnance')); ?> (<?= $this->game->line($game->line); ?>)
+	<div class="at"><br />at</div>
+	<div class="teams">
+		<?= $this->view()->render(array('element' => 'teams/pick_link'), array('game' => $game, 'team' => $game->awayTeam(), 'username' => 'jpnance')); ?><br /> 
+		<?= $this->view()->render(array('element' => 'teams/pick_link'), array('game' => $game, 'team' => $game->homeTeam(), 'username' => 'jpnance')); ?> (<?= $this->game->line($game->line); ?>)
+	</div>
 </div>
--- app/views/elements/games/started.html.php
+++ app/views/elements/games/started.html.php
@@ -1,4 +1,7 @@
 <div class="game started <?= $game->isPickedBy('jpnance') ? 'picked' : ''; ?>">
-	<span class="team"><?= $game->awayTeam()->location; ?></span><br />
-	<span class="team"><?= $game->homeTeam()->location; ?></span>
+	<div class="at"><br />at</div>
+	<div class="teams">
+		<span class="team"><?= $game->awayTeam()->location; ?></span><br />
+		<span class="team"><?= $game->homeTeam()->location; ?></span>
+	</div>
 </div>

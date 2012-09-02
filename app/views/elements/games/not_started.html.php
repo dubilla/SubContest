4272641540f3fc01bc6bf7c3b7a6feba2489c5bd
Patrick Nance <jpnance@gmail.com>
2012-09-02 15:21:50 -0700

    If the user isn't logged in, we won't show them clickable links.

--- app/views/elements/games/not_started.html.php
+++ app/views/elements/games/not_started.html.php
@@ -1,8 +1,14 @@
 <div id="<?= $game->_id; ?>" class="game not-started <?= ($game->isPickedBy($username) ? 'picked ' . ($game->picks[$username] == $game->awayTeam->abbreviation ? 'away-team' : 'home-team') : 'not-picked'); ?>">
 	<div class="at"><br />at</div>
 	<div class="teams">
-		<?= $this->view()->render(array('element' => 'teams/pick_link'), array('game' => $game, 'team' => $game->awayTeam(), 'username' => $username)); ?><br /> 
-		<?= $this->view()->render(array('element' => 'teams/pick_link'), array('game' => $game, 'team' => $game->homeTeam(), 'username' => $username)); ?>
+		<?php if (isset($username)): ?>
+			<?= $this->view()->render(array('element' => 'teams/pick_link'), array('game' => $game, 'team' => $game->awayTeam(), 'username' => $username)); ?><br /> 
+			<?= $this->view()->render(array('element' => 'teams/pick_link'), array('game' => $game, 'team' => $game->homeTeam(), 'username' => $username)); ?>
+		<?php else: ?>
+			<span class="team"><?= $this->team->name($game->awayTeam()); ?></span><br />
+			<span class="team"><?= $this->team->name($game->homeTeam()); ?></span>
+		<?php endif; ?>
+
 		<span class="line"><?= $this->game->line($game->line); ?></span>
 	</div>
 </div>

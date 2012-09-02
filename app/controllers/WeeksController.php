8756da9f046581c24feed41500dae82a0cdec206
Patrick Nance <jpnance@gmail.com>
2012-09-01 18:35:14 -0700

    Fixing a default case and only fetching the exact data we need. This is a security measure so people can't just ask for the JSON version of a week and see everybody's picks, locked or not.

--- app/controllers/WeeksController.php
+++ app/controllers/WeeksController.php
@@ -7,21 +7,23 @@ use app\models\Games;
 
 class WeeksController extends \lithium\action\Controller {
 
-	public function view($week) {
+	public function view($week = null) {
 		if (!isset($week)) {
 			$week = 1;
 		}
 
-		$conditions = array('week' => intval($week));
-		$order = array('kickoff' => 1, 'awayTeam.abbreviation' => 1);
-
-		$games = Games::all(compact('conditions', 'order'));
-
 		$user = Auth::check('default');
 		$username = $user['username'];
 
+		$conditions = array('week' => intval($week));
+		$order = array('kickoff' => 1, 'awayTeam.abbreviation' => 1);
+		$fields = array('awayTeam' => 1, 'homeTeam' => 1, 'kickoff' => 1, 'line' => 1, 'picks.' . $username => 1);
+
+		$games = Games::all(compact('conditions', 'order', 'fields'));
+
 		return compact('games', 'week', 'username');
 	}
+
 }
 
 ?>

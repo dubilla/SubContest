6914ec1446c4cd51f9435eb327c7b282b0ae2cfd
Patrick Nance <jpnance@gmail.com>
2012-09-11 18:21:36 -0400

    Adding a link to the spreadsheet until I can duplicate more of its functionality on the site.

--- app/views/layouts/default.html.php
+++ app/views/layouts/default.html.php
@@ -23,6 +23,7 @@
 			<p>
 				<?= $this->html->link('Games', '/'); ?> |
 				<?= $this->html->link('Standings', '/standings'); ?> |
+				<?= $this->html->link('Data', 'https://docs.google.com/spreadsheet/lv?key=0ArIlEyPlzLNIdHpoZnNJQ29xaHZ4STJXZWhuRUpqT0E'); ?> |
 
 				<?php if ($this->user->isLoggedIn()): ?>
 					<?= $this->html->link('Log Out', '/logout'); ?>

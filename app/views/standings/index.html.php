99cc2c01ac7fe0f0782d73e2918b6e6c3a4a8db8
Patrick Nance <jpnance@gmail.com>
2012-09-11 16:09:50 -0400

    Styling up the score column on the standings page.

--- app/views/standings/index.html.php
+++ app/views/standings/index.html.php
@@ -2,12 +2,12 @@
 <table id="standings">
 	<tr>
 		<th>Player</th>
-		<th>Score</th>
+		<th class="score">Score</th>
 	</tr>
 	<?php foreach ($standings as $username => $score): ?>
 		<tr>
 			<td><?= $username; ?></td>
-			<td><?= $this->standings->score($score); ?></td>
+			<td class="score"><?= $this->standings->score($score); ?></td>
 		</tr>
 	<?php endforeach; ?>
 </table>

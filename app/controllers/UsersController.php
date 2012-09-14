8545eea6d5490c30a188cccb19013311d65aa978
Patrick Nance <jpnance@gmail.com>
2012-09-13 20:40:17 -0700

    Accidentally committed this line some time ago. Reverting.

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -10,8 +10,6 @@ class UsersController extends \lithium\action\Controller {
 		$user = Users::create($this->request->data);
 
 		if ($this->request->data) {
-			$user->seasons = array(2011, floatval(2012), doubleval(2013));
-
 			if ($user->save()) {
 				$this->redirect('Users::add');
 			}

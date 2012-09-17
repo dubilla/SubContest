07055ebee49f56ab9acf4f6136fbaaf409a40bf1
Patrick Nance <jpnance@gmail.com>
2012-09-17 08:38:45 -0700

    Adding some CSS to make it clearer which team actually won/lost against the spread when you didn't pick the game.

--- app/views/elements/games/picks.html.php
+++ app/views/elements/games/picks.html.php
@@ -1,6 +1,6 @@
 <table class="picks">
 	<tr>
-		<th><?= $game->awayTeam->abbreviation; ?></th>
+		<th class="<?= isset($game->push) ? 'push' : ((isset($game->winner) && $game->winner == $game->awayTeam->abbreviation) ? 'winner' : 'loser'); ?>"><?= $game->awayTeam->abbreviation; ?></th>
 		<td>
 			<?php foreach ($game->awayTeam->picks as $name): ?>
 				<?= $name; ?>
@@ -8,7 +8,7 @@
 		</td>
 	</tr>
 	<tr>
-		<th><?= $game->homeTeam->abbreviation; ?></th>
+		<th class="<?= isset($game->push) ? 'push' : ((isset($game->winner) && $game->winner == $game->homeTeam->abbreviation) ? 'winner' : 'loser'); ?>"><?= $game->homeTeam->abbreviation; ?></th>
 		<td>
 			<?php foreach ($game->homeTeam->picks as $name): ?>
 				<?= $name; ?>

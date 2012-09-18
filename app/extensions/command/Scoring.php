ca4e773ca1051c08efa266ed34b3a21c800074b2
Patrick Nance <jpnance@gmail.com>
2012-09-18 08:11:15 -0700

    With the help of a wonderfully convenient XML feed NFL.com provides, we now have a batch script which will load in the final scores and figure out the winners/losers/pushers against the spread.

--- app/extensions/command/Scoring.php
+++ app/extensions/command/Scoring.php
@@ -6,7 +6,7 @@ use app\models\Games;
 
 class Scoring extends \lithium\console\Command {
 
-	public function run() {
+	public function determineWinners() {
 		$games = Games::all();
 
 		foreach ($games as $game) {
@@ -28,6 +28,55 @@ class Scoring extends \lithium\console\Command {
 				}
 			}
 		}
+	}
+
+	public function loadNflScores() {
+		$scores = file_get_contents('http://www.nfl.com/liveupdate/scorestrip/ss.xml');
+
+		$weekSeasonPattern = '/<gms .*?w="(\d\d?)".*?y="(\d\d\d\d)".*?>/';
+		preg_match($weekSeasonPattern, $scores, $weekSeasonMatches);
+		$week = intval($weekSeasonMatches[1]);
+		$season = intval($weekSeasonMatches[2]);
+
+		$gamePattern = '/<g .*?q="F".*?h="(.*?)".*?hs="(\d\d?)".*?v="(.*?)".*?vs="(\d\d?)".*?\/>/';
+		preg_match_all($gamePattern, $scores, $gameMatches);
+
+		foreach ($gameMatches[0] as $i => $gameMatch) {
+			$awayTeam = $gameMatches[3][$i];
+			$awayScore = intval($gameMatches[4][$i]);
+			$homeTeam = $gameMatches[1][$i];
+			$homeScore = intval($gameMatches[2][$i]);
+
+			$awayTeam = ($awayTeam == 'JAC') ? 'JAX' : $awayTeam;
+			$homeTeam = ($homeTeam == 'JAC') ? 'JAX' : $homeTeam;
+
+			$conditions = array(
+				'season' => $season,
+				'week' => $week,
+				'awayTeam.abbreviation' => $awayTeam,
+				'homeTeam.abbreviation' => $homeTeam,
+				'awayTeam.score' => array('$exists' => false),
+				'homeTeam.score' => array('$exists' => false),
+				'winner' => array('$exists' => false),
+				'push' => array('$exists' => false)
+			);
+
+			$game = Games::first(compact('conditions'));
+
+			if ($game) {
+				$game->awayTeam->score = $awayScore;
+				$game->homeTeam->score = $homeScore;
+
+				$game->save();
+			}
+		}
+
+		return;
+	}
+
+	public function run() {
+		$this->loadNflScores();
+		$this->determineWinners();
 	}
 
 }

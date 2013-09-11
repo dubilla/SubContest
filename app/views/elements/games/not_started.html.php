f38302092795f59c25b01e0dea18ae46b5d9a506
Patrick Nance <jpnance@gmail.com>
2013-09-11 13:40:34 -0700

    Logos should be shown to users aren't logged in.

--- app/views/elements/games/not_started.html.php
+++ app/views/elements/games/not_started.html.php
@@ -7,8 +7,8 @@
 			<div class="team away-team <?= $this->team->className($game->awayTeam()); ?>"><?= $this->view()->render(array('element' => 'teams/pick_link'), array('game' => $game, 'team' => $game->awayTeam(), 'username' => $username)); ?></div>
 			<div class="team home-team <?= $this->team->className($game->homeTeam()); ?>"><?= $this->view()->render(array('element' => 'teams/pick_link'), array('game' => $game, 'team' => $game->homeTeam(), 'username' => $username)); ?> <span class="line"><?= $this->game->line($game->line); ?></span></div>
 		<?php else: ?>
-			<div class="team"><?= $this->team->name($game->awayTeam()); ?></div>
-			<div class="team"><?= $this->team->name($game->homeTeam()); ?></div>
+			<div class="team <?= $this->team->className($game->awayTeam()); ?>"><?= $this->team->name($game->awayTeam()); ?></div>
+			<div class="team <?= $this->team->className($game->homeTeam()); ?>"><?= $this->team->name($game->homeTeam()); ?></div>
 		<?php endif; ?>
 	</div>
 </div>

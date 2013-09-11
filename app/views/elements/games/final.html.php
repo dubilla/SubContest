ff01da014e82dae02896b4a7148af6d0a499ec6b
Patrick Nance <jpnance@gmail.com>
2013-09-11 09:18:01 -0700

    A bunch more style changes. Let's omit the big stripe across the middle of each game.

--- app/views/elements/games/final.html.php
+++ app/views/elements/games/final.html.php
@@ -5,8 +5,8 @@
 		<div class="score"><?= $game->homeTeam->score; ?></div>
 	</div>
 	<div class="teams">
-		<div class="team <?= $game->isPickedBy($username) && $game->picks[$username] == $game->awayTeam->abbreviation ? 'pick' : ''; ?>"><?= $this->team->name($game->awayTeam()); ?></div>
-		<div class="team <?= $game->isPickedBy($username) && $game->picks[$username] == $game->homeTeam->abbreviation ? 'pick' : ''; ?>"><?= $this->team->name($game->homeTeam()); ?> <span class="line"><?= $this->game->line($game->line); ?></div>
+		<div class="team <?= $game->isPickedBy($username) && $game->picks[$username] == $game->awayTeam->abbreviation ? 'pick' : ''; ?> <?= $this->team->className($game->awayTeam()); ?>"><?= $this->team->name($game->awayTeam()); ?></div>
+		<div class="team <?= $game->isPickedBy($username) && $game->picks[$username] == $game->homeTeam->abbreviation ? 'pick' : ''; ?> <?= $this->team->className($game->homeTeam()); ?>"><?= $this->team->name($game->homeTeam()); ?> <span class="line"><?= $this->game->line($game->line); ?></div>
 	</div>
 	<?= $this->view()->render(array('element' => 'games/picks'), compact('game')); ?>
 </div>
